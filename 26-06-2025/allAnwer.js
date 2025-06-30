b0271fb412173909d69c66c32a26e5f51adb5b24
Rohit Kumar Sahoo <107879373+rohitsahoo5791@users.noreply.github.com>
2025-06-30 10:52:30 +0530

    Update allAnwer.js

--- 26-06-2025/allAnwer.js
+++ 26-06-2025/allAnwer.js
@@ -1,1 +1,78 @@
 
+// let nameR= "rohit"
+// let rev=""
+// for (let i=nameR.length -1;i!=-1;i--){
+//     rev+=nameR[i]
+// }
+// console.log(rev);  --1
+
+
+// function isPallendrom(txt){
+//     let i=0,j=txt.length-1;
+//     while(i<=j){
+//         if(txt[i]!=txt[j]) return false
+//         i++,j--;
+//     }
+//     return true
+// }
+
+// console.log(isPallendrom("abx"))
+
+
+
+// function getLargestWord(str) {
+//     // Split the string into an array of words
+//     const words = str.split(" ");
+    
+//     // Initialize a variable to store the longest word
+//     let longest = "";
+
+//     // Loop through the array to find the longest word
+//     for (let word of words) {
+//         if (word.length > longest.length) {
+//             longest = word;
+//         }
+//     }
+
+//     return longest;
+// }
+
+// // Example usage:
+// const input = "JavaScript is an awesome programming language";
+// const largestWord = getLargestWord(input);
+// console.log("The largest word is:", largestWord);
+
+
+
+
+// arr1= [1,2,3,[5,6,7,[6,9]]]
+// function getTheArray(arr,temp){
+//     arr.forEach(element => { 
+//         if(Array.isArray(element)){
+//             return getTheArray(element,temp)
+//         }
+//         else{
+//             temp.push(element)
+
+//         }
+        
+//     });
+//     return temp
+// }
+
+// console.log(getTheArray(arr1,[]))
+
+
+
+// function removeDupilicate(arr){
+//     re new Set(arr);
+//     return c
+
+
+
+// }
+
+// console.log(removeDupilicate([5,7,8,8]))
+
+
+
